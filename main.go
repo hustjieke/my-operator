60a56311ad645d453b69af9f2d028e25147837a1
hustjieke <gaoriyao@126.com>
2021-06-07 11:55:52 +0800

    *.: add webhook
    [summary]
    links: https://book.kubebuilder.io/cronjob-tutorial/webhook-implementation.html
    
    here our group is "radondb" and kind is "Cluster"
    
    $ kubebuilder create webhook --group radondb --version v1 --kind Cluster --defaulting --programmatic-validation
    
    git status:
    ```
    	modified:   PROJECT
    	modified:   main.go
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	api/v1/cluster_webhook.go
    	api/v1/webhook_suite_test.go
    	config/certmanager/
    	config/default/manager_webhook_patch.yaml
    	config/default/webhookcainjection_patch.yaml
    	config/webhook/```

--- main.go
+++ main.go
@@ -85,6 +85,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Cluster")
 		os.Exit(1)
 	}
+	if err = (&radondbv1.Cluster{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Cluster")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
